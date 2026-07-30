b061de2eca3ce4dd0ad50ddcf4445ce3471c4e30
twd <viraj@thewebdecor.com>
2026-07-30 12:57:47 +0530

    day 26 test 2

--- src/context/WishlistContext.js
+++ src/context/WishlistContext.js
@@ -22,7 +22,7 @@ export function WishlistProvider({ children }) {
     } finally {
       setIsLoaded(true);
     }
-  }, []);
+  }, []); 
 
   // Persist to localStorage whenever it changes (but skip the initial mount)
   useEffect(() => {
